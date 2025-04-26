bde709b9363ad21fceac477f810497dc0dbb132e
berrisaur <146159831+berrisaur@users.noreply.github.com>
2025-04-26 00:21:11 -0700

    Spacing
    Fixed spacing issues, and made it easier to read.

--- imageDecoder.py
+++ imageDecoder.py
@@ -1,8 +1,14 @@
 from PIL import Image
+
 def binary_to_password(binary_data, delimiter = "#end#"): #takes the binary password that would be decoded and convert it back into characters to use.
-    chars = [binary_data[i:i+8] for i in range(0, len(binary_data), 8)] #breaks apart the binary into each character( 8 bit)
-    decoded = ''.join(chr(int(char, 2)) for char in chars) #converts the 8 bit into integers, then converts the intergers into characters.
-    password = decoded.split(delimiter)[0] #stop at the delimiter we set up, and splits it from the password we just decoded.
+    #breaks apart the binary into each character( 8 bit)
+    chars = [binary_data [i : i + 8] for i in range(0, len(binary_data), 8)] 
+
+    #converts the 8 bit into integers, then converts the intergers into characters.
+    decoded = ''.join(chr(int(char, 2)) for char in chars) 
+    
+    #stop at the delimiter we set up, and splits it from the password we just decoded.
+    password = decoded.split(delimiter)[0] 
     return password
 
 def decode_password_from_image(image_path): #decodes the encoded image, getting up binary data to convert into a password.
--- imageEncoder.py
+++ imageEncoder.py
@@ -1,9 +1,11 @@
 from PIL import Image
 
-def password_to_binary(password, delimiter ="#end#"):
+def password_to_binary(password, delimiter = "#end#"):
     # Converts the password to binary, with a delimiter to mark the end
     full_text = password + delimiter
-    return ''.join(format(ord(char), '08b') for char in full_text) #converts the new text into binary.
+    #converts the new text into binary and returns it. 
+    return ''.join(format(ord(char), '08b') for char in full_text) 
+
 def encode_password_in_image(image_path, password, output_path): #this will take in a password input, an image, and encode the picture, and create a new image.
     img = Image.open(image_path)
     img = img.convert('RGB')
