d492d6094542f3c3046f3d6458fb29c19a95798d
Davin <davint04@uw.edu>
2025-04-25 22:28:09 -0700

    ts pmo commit

---
+++ BinaryManipulation.py
@@ -0,0 +1,10 @@
+from PIL import Image
+
+imageInput = Image.open()
+with Image.open(imageInput) as imagePassword:
+    im = imagePassword.load()
+x = 0
+y = 0
+resWidth = imagePassword.width()
+resHeight = 
+pixelValues = imagePassword.getpixel()
